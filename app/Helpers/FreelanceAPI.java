d288efe7f575b24f5d71d6c7d33ff5bfa9adef39
Haitham Abdel-Salam <haithamhany7@gmail.com>
2022-04-12 16:19:27 -0400

    Removing Coffee script and adding 1 result at a time

--- app/Helpers/FreelanceAPI.java
+++ app/Helpers/FreelanceAPI.java
@@ -9,7 +9,8 @@ public enum FreelanceAPI {
     SEARCH_TERM("/api/projects/0.1/projects/active/?limit=10&job_details=true&full_description=true&query="),
     OWNER_PROFILE("/api/users/0.1/portfolios/?limit=10&compact=true&portfolio_details=true&user_details=true&user_qualification_details=true&user_jobs=true&user_portfolio_details=true&user_recommendations=true&count=true&user_profile_description=true&users[]="),
     WORD_STATS("/api/projects/0.1/projects/active/?limit=250&job_details=true&full_description=true&query="),
-    PROJECT_BY_ID("/api/projects/0.1/projects/");
+    PROJECT_BY_ID("/api/projects/0.1/projects/"),
+    SINGLE_RESULT("/api/projects/0.1/projects/active/?limit=1&job_details=true&full_description=true&query=");
 
     private String url;
 
